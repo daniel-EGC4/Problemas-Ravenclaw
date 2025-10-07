6a58c159c186aa46d8ecfab621056f986e08f8c8
daniel-EGC4 <danieleguerreroc12@gmail.com>
2025-10-06 19:50:11 -0600

    Create Problema 4.js

---
+++ Problema 4.js
@@ -0,0 +1,18 @@
+const recursos = [
+    { titulo: "JS Avanzado", creador: "Prof. Toledo", calificacion: 5, esPremium: true },
+    { titulo: "Introducción a HTML", creador: "Estefania", calificacion: 4, esPremium: false },
+    { titulo: "APIs para Adolescentes", creador: "NeoCoder", calificacion: 4, esPremium: true },
+    { titulo: "CSS Básico", creador: "Estefania", calificacion: 5, esPremium: false },
+    { titulo: "Algoritmos Ciberseguros", creador: "Prof. Toledo", calificacion: 3, esPremium: true }
+];
+
+const procesarBibliotecaPremium = (recursos) => {
+    const filtrar = recursos.filter((p) => p.esPremium === true);
+    const ordenar = filtrar.sort ((a, b) => b.calificacion - a.calificacion );
+    const rehacer = ordenar.map(({ titulo, creador: autor, calificacion }) => {
+      return { titulo, calificacion, autor };
+    });
+    return rehacer
+};
+
+console.log(procesarBibliotecaPremium(recursos))
